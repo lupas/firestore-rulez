cfc07699858935f81bae7593d7570cfa35fefd4f
Pascal <pascal@luther.ch>
2019-01-20 16:20:59 +0100

    added helper functions

--- firestore-rulez.js
+++ firestore-rulez.js
@@ -3,15 +3,17 @@ var concat = require('concat')
 const fs = require('fs')
 
 // Read all files in current directory
-const rulesSrc = './rules'
+const rulesSrc = './test'
 const filesArray = []
 fs.readdirSync(rulesSrc).forEach(file => {
   filesArray.push(`${rulesSrc}/${file}`)
 })
 
-// Add Header to start of array
+// Add Header to 2nd position of array
+filesArray.unshift('helperFunctions/index.rules')
+// Add Header to 1st postion of array
 filesArray.unshift('templates/header.rules')
-// Add Footer to end of array
+// Add Footer to lastz position of array
 filesArray.push('templates/footer.rules')
 
 concat(filesArray, 'firestore.rules')
