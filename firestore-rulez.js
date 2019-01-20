36b2a623435d12a5a7e73742f088270e52e2a4e3
Pascal <pascal@luther.ch>
2019-01-20 16:33:47 +0100

    using __dirname so correct local paths are used

--- firestore-rulez.js
+++ firestore-rulez.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
-var concat = require('concat')
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const concat = require('concat')
 const fs = require('fs')
 
 // Read all files in current directory
-const rulesSrc = './test'
+const rulesSrc = './rules'
 const filesArray = []
 fs.readdirSync(rulesSrc).forEach(file => {
   filesArray.push(`${rulesSrc}/${file}`)
 })
 
 // Add Header to 2nd position of array
-filesArray.unshift('helperFunctions/index.rules')
+filesArray.unshift(`${__dirname}/helperFunctions/index.rules`)
 // Add Header to 1st postion of array
-filesArray.unshift('templates/header.rules')
+filesArray.unshift(`${__dirname}/templates/header.rules`)
 // Add Footer to lastz position of array
-filesArray.push('templates/footer.rules')
+filesArray.push(`${__dirname}/templates/footer.rules`)
 
 concat(filesArray, 'firestore.rules')
