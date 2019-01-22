8ff65edbc20122b597dcaeb069b35838ac2fe740
Pascal <pascal@luther.ch>
2019-01-22 13:27:00 +0100

    (1) added success console.info, (2) added error message if rules folder is missing

--- firestore-rulez.js
+++ firestore-rulez.js
@@ -2,32 +2,53 @@
 const concat = require('concat')
 const fs = require('fs')
 
-// Get Config
-let helpersEnabled = false
-try {
-  const config = require('./rules/rulez.config.js')
-  helpersEnabled = config.helpers
-} catch {
-  // no config available
-}
+const standardErrorMessage = '❌ Creating firestore.rules failed.'
 
-// Read all files in current directory
-const rulesSrc = './rules'
-const filesArray = []
-fs.readdirSync(rulesSrc).forEach(file => {
-  if (file.includes('.rules')) {
-    filesArray.push(`${rulesSrc}/${file}`)
+const compileFirestoreRules = () => {
+  // Get Config
+  let helpersEnabled = false
+  try {
+    const config = require('./rules/rulez.config.js')
+    helpersEnabled = config.helpers
+  } catch {
+    // no config available
   }
-})
 
-// Add Helper Functions to 2nd position of array
-if (helpersEnabled) {
-  filesArray.unshift(`${__dirname}/helperFunctions/index.rules`)
+  // Read all files in current directory
+  const rulesSrc = './rules'
+  const filesArray = []
+  try {
+    fs.readdirSync(rulesSrc).forEach(file => {
+      if (file.includes('.rules')) {
+        filesArray.push(`${rulesSrc}/${file}`)
+      }
+    })
+  } catch (e) {
+    if (e.message.includes('./rules')) {
+      console.error(
+        standardErrorMessage,
+        '(Error: Could not find folder ./rules, did you create it in the right place?)'
+      )
+      return
+    }
+  }
+
+  // Add Helper Functions to 2nd position of array
+  if (helpersEnabled) {
+    filesArray.unshift(`${__dirname}/helperFunctions/index.rules`)
+  }
+
+  // Add Header to 1st postion of array
+  filesArray.unshift(`${__dirname}/templates/header.rules`)
+  // Add Footer to lastz position of array
+  filesArray.push(`${__dirname}/templates/footer.rules`)
+
+  try {
+    concat(filesArray, 'firestore.rules')
+  } catch (e) {
+    console.error(standardErrorMessage, e)
+  }
+  console.info(`✅ firestore.rules created successfully.`)
 }
 
-// Add Header to 1st postion of array
-filesArray.unshift(`${__dirname}/templates/header.rules`)
-// Add Footer to lastz position of array
-filesArray.push(`${__dirname}/templates/footer.rules`)
-
-concat(filesArray, 'firestore.rules')
+compileFirestoreRules()
